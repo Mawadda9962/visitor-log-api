b4f65033d7d63aed4bddfc73707e6d508bfe5d0b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-07-15 10:36:03 +0400

    Adding visitorController methods

--- visitorlog-Java/src/main/java/com/example/visitorlog/Java/Controller/VisitorController.java
+++ visitorlog-Java/src/main/java/com/example/visitorlog/Java/Controller/VisitorController.java
@@ -1,7 +1,28 @@
 package com.example.visitorlog.Java.Controller;
 
+import com.example.visitorlog.Java.Entity.Visitor;
+import com.example.visitorlog.Java.Service.VisitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class VisitorController {
+
+    @Autowired
+    VisitorService visitorService;
+
+
+    @GetMapping("/visitors")
+    public List<Visitor> getAllVisitors(){
+        return visitorService.getAllVisitors();
+    }
+
+    @GetMapping("/visirots/{id}")
+    public Visitor getVisitorById(@PathVariable Long id){
+        return visitorService.getVisitorById(id);
+    }
 }
