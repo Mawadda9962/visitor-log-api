74b5a354c8abc1c998e404e740423a26d7fd8251
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-07-15 10:03:02 +0400

    Adding deleteVisitor method

--- visitorlog-Java/src/main/java/com/example/visitorlog/Java/Service/VisitorService.java
+++ visitorlog-Java/src/main/java/com/example/visitorlog/Java/Service/VisitorService.java
@@ -7,11 +7,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class VisitorService {
 
     private List<Visitor> visitors = new ArrayList<>();
+
+    //automatically generating unique id
+    private final AtomicLong visitorId = new AtomicLong(1);
 
     public List<Visitor> getAllVisitors(){
         return visitors;
@@ -32,9 +36,28 @@ public class VisitorService {
     }
 
     public Visitor addVisitor(Visitor visitor){
-
-
+        visitor.setId(visitorId.getAndIncrement());
+        visitors.add(visitor);
+        return visitor;
     }
+
+    public void deleteVisitor (Long id){
+        Visitor visitorToDelete = null;
+
+        for (Visitor visitor : visitors){
+            if (visitor.getId().equals(id))
+                visitorToDelete = visitor;
+        }
+        if(visitorToDelete == null){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Visitor not found"
+            );
+        }
+        visitors.remove(visitorToDelete);
+    }
+
+}
 
 
 }
