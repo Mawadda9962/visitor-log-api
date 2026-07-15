31c2df390a9b6183683e806956f9d3da7be6a179
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-07-15 09:40:58 +0400

    Adding return all visitors method

--- visitorlog-Java/src/main/java/com/example/visitorlog/Java/Controller/VisitorController.java
+++ visitorlog-Java/src/main/java/com/example/visitorlog/Java/Controller/VisitorController.java
@@ -1,4 +1,7 @@
 package com.example.visitorlog.Java.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VisitorController {
 }
--- visitorlog-Java/src/main/java/com/example/visitorlog/Java/Service/VisitorService.java
+++ visitorlog-Java/src/main/java/com/example/visitorlog/Java/Service/VisitorService.java
@@ -1,4 +1,21 @@
 package com.example.visitorlog.Java.Service;
 
+import com.example.visitorlog.Java.Entity.Visitor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class VisitorService {
+
+    private List<Visitor> visitors = new ArrayList<>();
+
+    public List<Visitor> getAllVisitors(){
+        return visitors;
+    }
+
+    private Visitor getVisitorById(){
+       return visitors.stream()
+    }
 }
